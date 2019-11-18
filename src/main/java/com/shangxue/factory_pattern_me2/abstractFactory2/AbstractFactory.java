2c34ac3c4b71d59968af6378ca195cd5c661c51c
tf <111@qq.com>
2019-11-18 18:52:31 +0800

    完。但2设计得不是很完美

--- src/main/java/com/shangxue/factory_pattern_me2/abstractFactory2/AbstractFactory.java
+++ src/main/java/com/shangxue/factory_pattern_me2/abstractFactory2/AbstractFactory.java
@@ -3,10 +3,11 @@ package com.shangxue.factory_pattern_me2.abstractFactory2;
 
 
 public abstract class AbstractFactory {
-    //TODO
-    //正常调用逻辑应该是根据工厂得到光明牌工厂，然后根据光明牌工厂的到酸奶。代码需要改，逻辑不对，返回String不行，没有扩展性啥垃圾玩意儿
-        //① 大概改成这个模式https://blog.csdn.net/jerry11112/article/details/80618420
-        //② 增加各个工厂利弊总结，并且总体再归纳一篇博客
+
+    /**
+     *  就是 @return String 有点怪怪的   ？？？
+     */
+
 
     /**
      * 获得光明牌牛奶
--- src/main/java/com/shangxue/factory_pattern_me2/abstractFactory2/Test.java
+++ src/main/java/com/shangxue/factory_pattern_me2/abstractFactory2/Test.java
@@ -1,9 +1,19 @@
 package com.shangxue.factory_pattern_me2.abstractFactory2;
 
 public class Test {
+
+
+    public static void main(String[] args) {
+
+        AbstractFactory abstractFactory=new PureMilkFactory();//这里PureMilkFactory还可以再抽象吗？ 不然和工厂方法有啥区别呢，配置多会配错的这个弊端还在呀，只能说抽象工厂方法处理了品牌的填写，但是口味的填写是否还能再进化一下？
+        System.out.println(abstractFactory.getBrightDairy());
+
+
+        AbstractFactory abstractFactory2=new SourMilkFactory();
+        System.out.println(abstractFactory2.getMengniu());
+
+
+    }
 }
 
 
-/**
- * 代码不是很好，意思一下吧。因为我觉得，不仅蒙牛他们有个接口Milk2，口味，酸牛奶也应该有个接口Milk3（应该取名叫Taste 更好）
- */
